996fc968217791892214bccdc25858a3a1866aa8
hungdz369@gmail.com <hungdz369@gmail.com>
2024-06-08 06:27:35 +0700

    login ok

---
+++ Presentation/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuaMe88.Data;
+using SuaMe88.Services.UserServices;
+
+namespace SuaMe88.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var result = await _userService.Login(request);
+            return StatusCode(result.Status, result);
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,17 +1,25 @@
-using Application.Mappings;
+﻿using Application.Mappings;
 using Application.Settings;
 using Domain.Entities;
 using Infrastructure.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using SuaMe88.Services.UserServices;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var allowSpecificOrigins = "_allowSpecificOrigins";
 var sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//Register service IUSerservice and Userservice
+builder.Services.AddScoped<IUserService, UserService>();
+
 
 // Add services to the container.
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
 builder.Services.AddDbContext<SuaMe88Context>(options =>
         options.UseSqlServer(sqlConnectionString));
 builder.Services.AddControllers();
@@ -35,11 +43,14 @@ builder.Services.AddCors(options =>
                           policy.AllowAnyMethod();
                       });
 });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwagger();
 builder.Services.AddDependenceInjection();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddCustomAuthentication(builder.Configuration);
 //builder.Services.AddFirebase();
 
 var app = builder.Build();
@@ -53,6 +64,8 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 //app.UseJwt();
+app.UseAuthentication();
+
 
 app.UseAuthorization();
 

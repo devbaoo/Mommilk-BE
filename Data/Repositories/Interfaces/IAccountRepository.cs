49c4527ceec66bafc180eabed42f53801720ad6b
hungdz369@gmail.com <hungdz369@gmail.com>
2024-06-08 23:53:50 +0700

    first commit

--- Data/Repositories/Interfaces/IAccountRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Entities;
-
-namespace Data.Repositories.Interfaces
-{
-    public interface IAccountRepository : IRepository<Customer>
-    {
-        Task<Customer> GetCustomerByEmailAndPasswordAsync(string email, string password);
-    }
-}
--- Domain/Models/Auth/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SuaMe88.Data
-{
-    public class LoginRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}

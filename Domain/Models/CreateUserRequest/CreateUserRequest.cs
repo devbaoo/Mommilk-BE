046997019a5948527341da333f3977f42fb5aba4
hungdz369@gmail.com <hungdz369@gmail.com>
2024-06-09 14:21:22 +0700

    logintest3

---
+++ Domain/Models/CreateUserRequest/CreateUserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.CreateUserRequest
+{
+    internal class CreateUserRequest
+    {
+        public Guid UserID { get; set; } = Guid.NewGuid();
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int? RoleCode { get; set; }
+
+        public bool? IsActive { get; set; } = true;
+        public string Phone { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    }
+}

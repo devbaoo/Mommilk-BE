4dc04c2fa9d0bcec8fbb6b96011bcadf1095b376
Be4r1908 <truong1908thd@gmail.com>
2024-06-12 15:47:59 +0700

    up product

--- Data/Repositories/Interfaces/IProductRepository.cs
+++ Data/Repositories/Interfaces/IProductRepository.cs
@@ -1,4 +1,10 @@
-﻿using Domain.Entities;
+﻿using Data.Repositories.Implementations;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Data.Repositories.Interfaces
 {

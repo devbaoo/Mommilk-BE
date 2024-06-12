83a0fd31a788364c205ba9bf14fbff0213d519a9
hungdz369@gmail.com <hungdz369@gmail.com>
2024-06-12 17:10:07 +0700

    fix regis

--- Domain/Models/CreateUserRequest/CreateUserRequest.cs
+++ Domain/Models/CreateUserRequest/CreateUserRequest.cs
@@ -5,17 +5,19 @@ namespace Domain.Models.CreateUserRequest
 {
     public class CreateUserRequest
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-
+/*        public Guid Id { get; set; } = Guid.NewGuid();
+*/
+        
+        public string Name { get; set; }
         [Display(Name = "Email address")]
         [Required(ErrorMessage = "The email address is required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; }
-        public string Name { get; set; }
-        public string AvatarUrl { get; set; }
-        public string Rank { get; set; }
-        public bool? Status { get; set; } = true;
-    }
+        
+/*        public string AvatarUrl { get; set; }
+        public string Rank { get; set; }*/
+/*        public bool? Status { get; set; } = true;
+*/    }
 }

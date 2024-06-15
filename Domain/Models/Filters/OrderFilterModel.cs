629f37d043ed1124625cd798cf6002867da8c2e8
hungdz369@gmail.com <hungdz369@gmail.com>
2024-06-15 12:17:28 +0700

    xoa code

--- Domain/Models/Filters/OrderFilterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Models.Filters
-{
-    public class OrderFilterModel
-    {
-        public Guid? Id { get; set; }
-        public Guid? CustomerId { get; set; }
-        public DateTime? DeliveryDate { get; set; }
-    }
-}
--- Domain/Models/Pagination/PaginationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Models.Pagination
-{
-    public class PaginationViewModel
-    {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-        public int TotalRows { get; set; }
-    }
-}
